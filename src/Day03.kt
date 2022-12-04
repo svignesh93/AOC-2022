b37c673287b804fe40dc37c26ca9414ae6f0bfdc
svignesh93 <vignesh.vic727@gmail.com>
2022-12-05 04:16:44 +0530

    Solved advent of code 2022 Day 04 puzzle

--- src/Day03.kt
+++ src/Day03.kt
@@ -94,16 +94,16 @@
  *
  * Find the item type that corresponds to the badges of each three-Elf group.
  * What is the sum of the priorities of those item types?
+ *
+ * Your puzzle answer was 2363.
  */
 fun main() {
-    val priorityMap = mapOf(
-        'a' to 1, 'b' to 2, 'c' to 3, 'd' to 4, 'e' to 5, 'f' to 6, 'g' to 7, 'h' to 8, 'i' to 9, 'j' to 10, 'k' to 11,
-        'l' to 12, 'm' to 13, 'n' to 14, 'o' to 15, 'p' to 16, 'q' to 17, 'r' to 18, 's' to 19, 't' to 20, 'u' to 21,
-        'v' to 22, 'w' to 23, 'x' to 24, 'y' to 25, 'z' to 26, 'A' to 27, 'B' to 28, 'C' to 29, 'D' to 30, 'E' to 31,
-        'F' to 32, 'G' to 33, 'H' to 34, 'I' to 35, 'J' to 36, 'K' to 37, 'L' to 38, 'M' to 39, 'N' to 40, 'O' to 41,
-        'P' to 42, 'Q' to 43, 'R' to 44, 'S' to 45, 'T' to 46, 'U' to 47, 'V' to 48, 'W' to 49, 'X' to 50, 'Y' to 51,
-        'Z' to 52
-    )
+
+    fun Char.priority() = when (this) {
+        in 'a'..'z' -> this - 'a' + 1
+        in 'A'..'Z' -> this - 'A' + 27
+        else -> 0
+    }
 
     val input = readInput("Day03")
 
@@ -116,7 +116,7 @@ fun main() {
             for (itemIndex in secondCompartment.indices) {
                 val item = secondCompartment[itemIndex]
                 if (firstCompartment.contains(item)) {
-                    prioritySum += priorityMap[item] ?: 0
+                    prioritySum += item.priority()
                     break
                 }
             }
@@ -143,7 +143,7 @@ fun main() {
             for (itemIndex in rucksackThree.indices) {
                 val item = rucksackThree[itemIndex]
                 if (commonItemSet.contains(item)) {
-                    prioritySum += priorityMap[item] ?: 0
+                    prioritySum += item.priority()
                     break
                 }
             }
